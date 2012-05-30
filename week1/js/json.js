87222f8624d801a1f794f47c418c9ea724961d36
mikesmithjr <mikesmithjr@fullsail.edu>
2012-05-30 00:05:00 -0500

    Adjusting some CSS and JSON and changing the index.html page.  Then creating folders for following weeks.

--- week1/js/json.js
+++ week1/js/json.js
@@ -4,10 +4,11 @@
 
 
 var json = {
+
 	"logItem1": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/10/2012"],
 		"currentTime": ["Current Time:", "08:00"],
 		"bsreading": ["Blood Sugar Reading:", "125"],
 		"sex": ["Male or Female:", "Male"],
@@ -19,7 +20,7 @@ var json = {
 	"logItem2": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/11/2012"],
 		"currentTime": ["Current Time:", "12:00"],
 		"bsreading": ["Blood Sugar Reading:", "75"],
 		"sex": ["Male or Female:", "Male"],
@@ -43,7 +44,7 @@ var json = {
 	"logItem4": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/13/2012"],
 		"currentTime": ["Current Time:", "19:00"],
 		"bsreading": ["Blood Sugar Reading:", "115"],
 		"sex": ["Male or Female:", "Male"],
@@ -51,10 +52,11 @@ var json = {
 		"treatments": ["Current Treatment:", "Insulin"],
 		"comments":	["Comments:", "Feeling Great!!"]
 	},
+	
 	"logItem5": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/13/2012"],
+		"date": ["Today's Date:", "05/14/2012"],
 		"currentTime": ["Current Time:", "18:00"],
 		"bsreading": ["Blood Sugar Reading:", "125"],
 		"sex": ["Male or Female:", "Male"],
@@ -62,12 +64,13 @@ var json = {
 		"treatments": ["Current Treatment:", "Pills"],
 		"comments":	["Comments:", "Feeling Awesome!!"]
 	},
+	
 	"logItem6": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/15/2012"],
 		"currentTime": ["Current Time:", "08:00"],
-		"bsreading": ["Blood Sugar Reading:", "125"],
+		"bsreading": ["Blood Sugar Reading:", "135"],
 		"sex": ["Male or Female:", "Male"],
 		"condition": ["Condition:", "7"],
 		"treatments": ["Current Treatment:", "Pills"],
@@ -77,7 +80,7 @@ var json = {
 	"logItem7": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/16/2012"],
 		"currentTime": ["Current Time:", "12:00"],
 		"bsreading": ["Blood Sugar Reading:", "75"],
 		"sex": ["Male or Female:", "Male"],
@@ -89,9 +92,9 @@ var json = {
 	"logItem8": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/17/2012"],
 		"currentTime": ["Current Time:", "16:00"],
-		"bsreading": ["Blood Sugar Reading:", "165"],
+		"bsreading": ["Blood Sugar Reading:", "145"],
 		"sex": ["Male or Female:", "Male"],
 		"condition": ["Condition:", "5"],
 		"treatments": ["Current Treatment:", "Pills"],
@@ -101,7 +104,7 @@ var json = {
 	"logItem9": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/18/2012"],
 		"currentTime": ["Current Time:", "19:00"],
 		"bsreading": ["Blood Sugar Reading:", "115"],
 		"sex": ["Male or Female:", "Male"],
@@ -109,10 +112,11 @@ var json = {
 		"treatments": ["Current Treatment:", "Insulin"],
 		"comments":	["Comments:", "Feeling Great!!"]
 	},
+	
 	"logItem10": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/13/2012"],
+		"date": ["Today's Date:", "05/19/2012"],
 		"currentTime": ["Current Time:", "18:00"],
 		"bsreading": ["Blood Sugar Reading:", "125"],
 		"sex": ["Male or Female:", "Male"],
@@ -124,7 +128,7 @@ var json = {
 	"logItem11": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/20/2012"],
 		"currentTime": ["Current Time:", "08:00"],
 		"bsreading": ["Blood Sugar Reading:", "125"],
 		"sex": ["Male or Female:", "Male"],
@@ -136,9 +140,9 @@ var json = {
 	"logItem12": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/21/2012"],
 		"currentTime": ["Current Time:", "12:00"],
-		"bsreading": ["Blood Sugar Reading:", "75"],
+		"bsreading": ["Blood Sugar Reading:", "55"],
 		"sex": ["Male or Female:", "Male"],
 		"condition": ["Condition:", "4"],
 		"treatments": ["Current Treatment:", "Insulin"],
@@ -148,32 +152,33 @@ var json = {
 	"logItem13": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/22/2012"],
 		"currentTime": ["Current Time:", "16:00"],
-		"bsreading": ["Blood Sugar Reading:", "165"],
+		"bsreading": ["Blood Sugar Reading:", "195"],
 		"sex": ["Male or Female:", "Male"],
 		"condition": ["Condition:", "5"],
 		"treatments": ["Current Treatment:", "Pills"],
-		"comments":	["Comments:", "Feeling OK."]
+		"comments":	["Comments:", "Feeling sick."]
 	},
 	
 	"logItem14": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/23/2012"],
 		"currentTime": ["Current Time:", "19:00"],
-		"bsreading": ["Blood Sugar Reading:", "115"],
+		"bsreading": ["Blood Sugar Reading:", "105"],
 		"sex": ["Male or Female:", "Male"],
 		"condition": ["Condition:", "8"],
 		"treatments": ["Current Treatment:", "Insulin"],
 		"comments":	["Comments:", "Feeling Great!!"]
 	},
+	
 	"logItem15": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/13/2012"],
+		"date": ["Today's Date:", "05/24/2012"],
 		"currentTime": ["Current Time:", "18:00"],
-		"bsreading": ["Blood Sugar Reading:", "125"],
+		"bsreading": ["Blood Sugar Reading:", "145"],
 		"sex": ["Male or Female:", "Male"],
 		"condition": ["Condition:", "10"],
 		"treatments": ["Current Treatment:", "Pills"],
@@ -183,9 +188,9 @@ var json = {
 	"logItem16": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/25/2012"],
 		"currentTime": ["Current Time:", "08:00"],
-		"bsreading": ["Blood Sugar Reading:", "125"],
+		"bsreading": ["Blood Sugar Reading:", "130"],
 		"sex": ["Male or Female:", "Male"],
 		"condition": ["Condition:", "7"],
 		"treatments": ["Current Treatment:", "Pills"],
@@ -195,9 +200,9 @@ var json = {
 	"logItem17": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/26/2012"],
 		"currentTime": ["Current Time:", "12:00"],
-		"bsreading": ["Blood Sugar Reading:", "75"],
+		"bsreading": ["Blood Sugar Reading:", "60"],
 		"sex": ["Male or Female:", "Male"],
 		"condition": ["Condition:", "4"],
 		"treatments": ["Current Treatment:", "Insulin"],
@@ -207,9 +212,9 @@ var json = {
 	"logItem18": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/27/2012"],
 		"currentTime": ["Current Time:", "16:00"],
-		"bsreading": ["Blood Sugar Reading:", "165"],
+		"bsreading": ["Blood Sugar Reading:", "125"],
 		"sex": ["Male or Female:", "Male"],
 		"condition": ["Condition:", "5"],
 		"treatments": ["Current Treatment:", "Pills"],
@@ -219,20 +224,21 @@ var json = {
 	"logItem19": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/12/2012"],
+		"date": ["Today's Date:", "05/28/2012"],
 		"currentTime": ["Current Time:", "19:00"],
-		"bsreading": ["Blood Sugar Reading:", "115"],
+		"bsreading": ["Blood Sugar Reading:", "110"],
 		"sex": ["Male or Female:", "Male"],
 		"condition": ["Condition:", "8"],
 		"treatments": ["Current Treatment:", "Insulin"],
 		"comments":	["Comments:", "Feeling Great!!"]
 	},
+	
 	"logItem20": {
 		"fname": ["First Name:", "Mike"],
 		"lname": ["Last Name:", "Smith"],
-		"date": ["Today's Date:", "05/13/2012"],
+		"date": ["Today's Date:", "05/29/2012"],
 		"currentTime": ["Current Time:", "18:00"],
-		"bsreading": ["Blood Sugar Reading:", "125"],
+		"bsreading": ["Blood Sugar Reading:", "100"],
 		"sex": ["Male or Female:", "Male"],
 		"condition": ["Condition:", "10"],
 		"treatments": ["Current Treatment:", "Pills"],
