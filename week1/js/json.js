ba6dc6f75a206c4322482ac56a481d264e46c9a3
mikesmithjr <mikesmithjr@fullsail.edu>
2012-05-29 20:25:59 -0500

    Adding extra JSON data and changing comments to update for this month's class.

--- week1/js/json.js
+++ week1/js/json.js
@@ -1,4 +1,4 @@
-//Project 4 Visual Frameworks Term 1205
+//Project 1 MIU Term 1206
 //Michael Smith Jr.
 //The Diabetic Blood Sugar Log
 
@@ -62,7 +62,184 @@ var json = {
 		"treatments": ["Current Treatment:", "Pills"],
 		"comments":	["Comments:", "Feeling Awesome!!"]
 	}
+	"logItem6": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "08:00"],
+		"bsreading": ["Blood Sugar Reading:", "125"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "7"],
+		"treatments": ["Current Treatment:", "Pills"],
+		"comments":	["Comments:", "Feeling Great Today!!"]
+	},
+
+	"logItem7": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "12:00"],
+		"bsreading": ["Blood Sugar Reading:", "75"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "4"],
+		"treatments": ["Current Treatment:", "Insulin"],
+		"comments":	["Comments:", "Not feeling well."]
+	},
 	
+	"logItem8": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "16:00"],
+		"bsreading": ["Blood Sugar Reading:", "165"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "5"],
+		"treatments": ["Current Treatment:", "Pills"],
+		"comments":	["Comments:", "Feeling OK."]
+	},
+	
+	"logItem9": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "19:00"],
+		"bsreading": ["Blood Sugar Reading:", "115"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "8"],
+		"treatments": ["Current Treatment:", "Insulin"],
+		"comments":	["Comments:", "Feeling Great!!"]
+	},
+	"logItem10": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/13/2012"],
+		"currentTime": ["Current Time:", "18:00"],
+		"bsreading": ["Blood Sugar Reading:", "125"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "10"],
+		"treatments": ["Current Treatment:", "Pills"],
+		"comments":	["Comments:", "Feeling Awesome!!"]
+	}
+	
+	"logItem11": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "08:00"],
+		"bsreading": ["Blood Sugar Reading:", "125"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "7"],
+		"treatments": ["Current Treatment:", "Pills"],
+		"comments":	["Comments:", "Feeling Great Today!!"]
+	},
+
+	"logItem12": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "12:00"],
+		"bsreading": ["Blood Sugar Reading:", "75"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "4"],
+		"treatments": ["Current Treatment:", "Insulin"],
+		"comments":	["Comments:", "Not feeling well."]
+	},
+	
+	"logItem13": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "16:00"],
+		"bsreading": ["Blood Sugar Reading:", "165"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "5"],
+		"treatments": ["Current Treatment:", "Pills"],
+		"comments":	["Comments:", "Feeling OK."]
+	},
+	
+	"logItem14": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "19:00"],
+		"bsreading": ["Blood Sugar Reading:", "115"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "8"],
+		"treatments": ["Current Treatment:", "Insulin"],
+		"comments":	["Comments:", "Feeling Great!!"]
+	},
+	"logItem15": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/13/2012"],
+		"currentTime": ["Current Time:", "18:00"],
+		"bsreading": ["Blood Sugar Reading:", "125"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "10"],
+		"treatments": ["Current Treatment:", "Pills"],
+		"comments":	["Comments:", "Feeling Awesome!!"]
+	}
+	
+	"logItem16": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "08:00"],
+		"bsreading": ["Blood Sugar Reading:", "125"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "7"],
+		"treatments": ["Current Treatment:", "Pills"],
+		"comments":	["Comments:", "Feeling Great Today!!"]
+	},
+
+	"logItem17": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "12:00"],
+		"bsreading": ["Blood Sugar Reading:", "75"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "4"],
+		"treatments": ["Current Treatment:", "Insulin"],
+		"comments":	["Comments:", "Not feeling well."]
+	},
+	
+	"logItem18": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "16:00"],
+		"bsreading": ["Blood Sugar Reading:", "165"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "5"],
+		"treatments": ["Current Treatment:", "Pills"],
+		"comments":	["Comments:", "Feeling OK."]
+	},
+	
+	"logItem19": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/12/2012"],
+		"currentTime": ["Current Time:", "19:00"],
+		"bsreading": ["Blood Sugar Reading:", "115"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "8"],
+		"treatments": ["Current Treatment:", "Insulin"],
+		"comments":	["Comments:", "Feeling Great!!"]
+	},
+	"logItem20": {
+		"fname": ["First Name:", "Mike"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Today's Date:", "05/13/2012"],
+		"currentTime": ["Current Time:", "18:00"],
+		"bsreading": ["Blood Sugar Reading:", "125"],
+		"sex": ["Male or Female:", "Male"],
+		"condition": ["Condition:", "10"],
+		"treatments": ["Current Treatment:", "Pills"],
+		"comments":	["Comments:", "Feeling Awesome!!"]
+	}
+	
+
 
 
 }
